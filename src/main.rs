41f566bc77995c9bdf4be41b90a7b761042a23d3
Robert Williams <1266467+bobdoah@users.noreply.github.com>
2025-10-26 22:10:59 +0000

    feat: do rgb magic

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,63 @@
 #![no_std]
 #![no_main]
 
+use defmt::*;
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
+use embassy_rp::bind_interrupts;
+use embassy_rp::peripherals::PIO0;
+use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_rp::pio_programs::ws2812::{PioWs2812, PioWs2812Program};
+use embassy_time::{Duration, Ticker};
+use smart_leds::RGB8;
 use {defmt_rtt as _, panic_probe as _};
 
-// Program metadata for `picotool info`.
-// This isn't needed, but it's recommended to have these minimal entries.
-#[unsafe(link_section = ".bi_entries")]
-#[used]
-pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr 4] = [
-    embassy_rp::binary_info::rp_program_name!(c"chroma crab"),
-    embassy_rp::binary_info::rp_program_description!(
-        c"This runs light patterns on a Pimoroni Plasma 2350 board"
-    ),
-    embassy_rp::binary_info::rp_cargo_version!(),
-    embassy_rp::binary_info::rp_program_build_attribute!(),
-];
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
+
+/// Input a value 0 to 255 to get a color value
+/// The colours are a transition r - g - b - back to r.
+fn wheel(mut wheel_pos: u8) -> RGB8 {
+    wheel_pos = 255 - wheel_pos;
+    if wheel_pos < 85 {
+        return (255 - wheel_pos * 3, 0, wheel_pos * 3).into();
+    }
+    if wheel_pos < 170 {
+        wheel_pos -= 85;
+        return (0, wheel_pos * 3, 255 - wheel_pos * 3).into();
+    }
+    wheel_pos -= 170;
+    (wheel_pos * 3, 255 - wheel_pos * 3, 0).into()
+}
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) -> ! {
-    let periphs = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_16, Level::Low);
+async fn main(_spawner: Spawner) {
+    info!("Start");
+    let p = embassy_rp::init(Default::default());
 
+    let Pio {
+        mut common, sm0, ..
+    } = Pio::new(p.PIO0, Irqs);
+
+    const NUM_LEDS: usize = 50;
+    let mut data = [RGB8::default(); NUM_LEDS];
+
+    let program = PioWs2812Program::new(&mut common);
+    let mut ws2812 = PioWs2812::new(&mut common, sm0, p.DMA_CH0, p.PIN_15, &program);
+
+    // Loop forever making RGB values and pushing them out to the WS2812.
+    let mut ticker = Ticker::every(Duration::from_millis(10));
     loop {
-        defmt::info!("Blink on");
-        led.set_high();
-        Timer::after(Duration::from_millis(500)).await;
+        for j in 0..(256 * 5) {
+            debug!("New Colors:");
+            for i in 0..NUM_LEDS {
+                data[i].r = 255;
+                //data[i] = wheel((((i * 256) as u16 / NUM_LEDS as u16 + j as u16) & 255) as u8);
+                debug!("R: {} G: {} B: {}", data[i].r, data[i].g, data[i].b);
+            }
+            ws2812.write(&data).await;
 
-        defmt::info!("Blink off");
-        led.set_low();
-        Timer::after(Duration::from_millis(500)).await;
+            ticker.next().await;
+        }
     }
 }
