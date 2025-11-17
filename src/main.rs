42d43f115358e35a74ca12b281f3e6ff6360e8a4
Robert Williams <1266467+bobdoah@users.noreply.github.com>
2025-11-17 22:07:32 +0000

    feat: defmt usb serial logging

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,24 @@ use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Input, Pull};
-use embassy_rp::peripherals::{PIO0, TRNG};
+use embassy_rp::peripherals::{PIO0, TRNG, USB};
 use embassy_rp::pio::{InterruptHandler, Pio};
 use embassy_rp::pio_programs::ws2812::{PioWs2812, PioWs2812Program, Rgb};
+use embassy_rp::Peri;
 use embassy_sync::{
     blocking_mutex::raw::CriticalSectionRawMutex,
     channel::{Channel, Receiver, Sender},
 };
 use embassy_time::Ticker;
-use patterns::{rainbow, twinkle, twinkle::TwinkleState, Pattern};
+use patterns::{rainbow, twinkle, Pattern};
 use smart_leds::RGB8;
 
-use {defmt_rtt as _, panic_probe as _};
+use panic_probe as _;
 
 bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => InterruptHandler<PIO0>;
     TRNG_IRQ => embassy_rp::trng::InterruptHandler<TRNG>;
+    USBCTRL_IRQ => embassy_rp::usb::InterruptHandler<USB>;
 });
 
 mod button;
@@ -45,13 +47,7 @@ async fn main(spawner: Spawner) {
     let mut ws2812: PioWs2812<'_, _, 0, 50, Rgb> =
         PioWs2812::with_color_order(&mut common, sm0, p.DMA_CH0, p.PIN_15, &program);
 
-    let mut randomness = [0u8; 2 * NUM_LEDS];
-    let trng = embassy_rp::trng::Trng::new(p.TRNG, Irqs, Default::default());
-    let mut twinkle_state = TwinkleState {
-        trng,
-        randomness: &mut randomness,
-        rand_index: 0,
-    };
+    let mut trng = embassy_rp::trng::Trng::new(p.TRNG, Irqs, Default::default());
     let mut rainbow_counter: u16 = 0;
 
     let mut current_pattern = Pattern::Rainbow;
@@ -62,6 +58,7 @@ async fn main(spawner: Spawner) {
 
     #[allow(clippy::unwrap_used)]
     spawner.spawn(button_task(sender, btn_a).unwrap());
+    spawner.spawn(defmtusb_wrapper(p.USB).unwrap());
 
     let receiver: Receiver<'static, CriticalSectionRawMutex, (), 1> = CHANNEL.receiver();
 
@@ -84,11 +81,27 @@ async fn main(spawner: Spawner) {
                     rainbow::generate(&mut data, &mut rainbow_counter);
                 }
                 Pattern::Twinkle => {
-                    twinkle::generate(&mut data, &mut twinkle_state).await;
+                    twinkle::generate(&mut data, &mut trng).await;
                 }
             }
             ws2812.write(&data).await;
             ticker.next().await;
         }
     }
+}
+
+#[embassy_executor::task]
+async fn defmtusb_wrapper(usb: Peri<'static, USB>) {
+    let driver = embassy_rp::usb::Driver::new(usb, Irqs);
+    let config = {
+        let mut c = embassy_usb::Config::new(0x1234, 0x5678);
+        c.serial_number = Some("defmt");
+        c.max_packet_size_0 = 64;
+        c.composite_with_iads = true;
+        c.device_class = 0xEF;
+        c.device_sub_class = 0x02;
+        c.device_protocol = 0x01;
+        c
+    };
+    defmt_embassy_usbserial::run(driver, config).await;
 }
