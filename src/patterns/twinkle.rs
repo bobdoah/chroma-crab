300f3230a7f780b23d8eaaa18d7c5942920d1efe
Robert Williams <1266467+bobdoah@users.noreply.github.com>
2025-11-17 22:06:48 +0000

    feat: make twinkle less rng

--- src/patterns/twinkle.rs
+++ src/patterns/twinkle.rs
@@ -4,47 +4,28 @@ use embassy_rp::trng::Trng;
 use embassy_time::Duration;
 use smart_leds::RGB8;
 
-const TWINKLE_THRESHOLD: u8 = 4;
 const FADE_AMOUNT: u8 = 8;
-pub const DURATION: Duration = Duration::from_millis(25);
 
-pub struct TwinkleState<'a> {
-    pub trng: Trng<'a, TRNG>,
-    pub randomness: &'a mut [u8],
-    pub rand_index: usize,
-}
+pub const DURATION: Duration = Duration::from_millis(2);
 
-impl<'a> TwinkleState<'a> {
-    /// Refills the random buffer with fresh TRNG data.
-    pub async fn replenish_randomness(&mut self) {
-        self.trng.fill_bytes(self.randomness).await;
-        self.rand_index = 0;
-    }
-}
+pub async fn generate(data: &mut [RGB8], trng: &mut Trng<'static, TRNG>) {
+    let mut random_bytes = [0u8; 10];
+    trng.fill_bytes(&mut random_bytes).await;
 
-pub async fn generate(data: &mut [RGB8], state: &mut TwinkleState<'_>) {
-    state.replenish_randomness().await;
-    let randomness = &state.randomness;
-
+    // Dim all the LEDs
     for i in 0..data.len() {
         let current_color = data[i];
 
-        // Dim the LED
         data[i] = RGB8 {
             r: current_color.r.saturating_sub(FADE_AMOUNT),
             g: current_color.g.saturating_sub(FADE_AMOUNT),
             b: current_color.b.saturating_sub(FADE_AMOUNT),
         };
+    }
 
-        let twinkle_check = randomness[state.rand_index];
-        state.rand_index += 1;
-
-        // Or Twinkle
-        if twinkle_check < (256 / TWINKLE_THRESHOLD as u16) as u8 {
-            let random_wheel_pos = randomness[state.rand_index];
-            state.rand_index += 1;
-
-            data[i] = wheel(random_wheel_pos);
-        }
+    // Pick some random LEDs to Twinkle
+    for i in 0..(random_bytes.len() / 2) {
+        let led_index = (random_bytes[i * 2] as usize) % data.len();
+        data[led_index] = wheel(random_bytes[i * 2 + 1]);
     }
 }
