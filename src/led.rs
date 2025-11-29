96a00d6f4ca22a181dba9882eabbf184ac457c8e
Robert Williams <1266467+bobdoah@users.noreply.github.com>
2025-11-29 10:52:32 +0000

    fix: integer overflow (duh), and handle errors a bit

--- src/led.rs
+++ src/led.rs
@@ -1,5 +1,6 @@
+use defmt::{error, info, Debug2Format};
 use embassy_rp::peripherals::{PIN_16, PIN_17, PIN_18, PWM_SLICE0, PWM_SLICE1};
-use embassy_rp::pwm::{Config, Pwm, SetDutyCycle};
+use embassy_rp::pwm::{Config, Pwm, PwmError, SetDutyCycle};
 use embassy_rp::Peri;
 use embassy_time::Timer;
 use smart_leds::RGB8;
@@ -37,29 +38,56 @@ impl<'d> RGBLed<'d> {
         }
     }
 
-    fn set_color(&mut self, color: RGB8) {
+    fn set_color(&mut self, color: RGB8) -> Result<(), PwmError> {
         let (r_opt, g_opt) = self.pwm_rg.split_by_ref();
 
         let mut pwm_r = r_opt.unwrap();
         let mut pwm_g = g_opt.unwrap();
 
         // LED is active low
-        let r_duty = ((255 - color.r) as u16 * self.top) / 255 as u16;
-        let g_duty = ((255 - color.g) as u16 * self.top) / 255 as u16;
-        let b_duty = ((255 - color.b) as u16 * self.top) / 255 as u16;
+        let r_duty = (((255 - color.r) as u32 * self.top as u32) / 255) as u16;
+        let g_duty = (((255 - color.g) as u32 * self.top as u32) / 255) as u16;
+        let b_duty = (((255 - color.b) as u32 * self.top as u32) / 255) as u16;
+        info!("Setting color to {}", Debug2Format(&color));
+        info!("top: {}", self.top);
+        info!("r duty: {}", r_duty);
+        info!(
+            "r_duty: {}, g_duty: {}, b_duty: {}, top: {}",
+            r_duty, g_duty, b_duty, self.top
+        );
 
-        pwm_r.set_duty_cycle(r_duty).unwrap();
-        pwm_g.set_duty_cycle(g_duty).unwrap();
-        self.pwm_b.set_duty_cycle(b_duty).unwrap();
+        pwm_r.set_duty_cycle(r_duty)?;
+        pwm_g.set_duty_cycle(g_duty)?;
+        self.pwm_b.set_duty_cycle(b_duty)?;
+
+        Ok(())
     }
 }
 
 #[embassy_executor::task]
 pub async fn led_task(mut rgb_led: RGBLed<'static>) {
+    info!("Starting LED task");
     loop {
-        rgb_led.set_color(smart_leds::colors::INDIGO);
+        match rgb_led.set_color(smart_leds::colors::INDIGO) {
+            Ok(_) => {}
+            Err(e) => {
+                error!(
+                    "Failed to set INDIGO color (Duty Cycle Error): {:?}",
+                    Debug2Format(&e)
+                );
+            }
+        }
         Timer::after_millis(500).await;
-        rgb_led.set_color(smart_leds::colors::SNOW);
+        match rgb_led.set_color(smart_leds::colors::BLACK) {
+            Ok(_) => {}
+            Err(e) => {
+                error!(
+                    "Failed to set BLACK color (Duty Cycle Error): {:?}",
+                    Debug2Format(&e)
+                );
+            }
+        }
+
         Timer::after_millis(500).await;
     }
 }
